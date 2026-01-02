8668270141843ce44ba19696bcf77e662ea76c8e
kmnakane <18519798091@163.com>
2026-01-03 00:34:32 +0800

    部署了umami统计模块

--- src/config.ts
+++ src/config.ts
@@ -664,10 +664,10 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+	enabled: true, // 是否显示Umami统计
+	apiKey: import.meta.env.UMAMI_API_KEY || "api_04j2aqh7QIYdVm6UhPKd4rMkKBdZy3BT", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="ee228caf-e3e1-4603-a56c-a12facf8adcc"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
