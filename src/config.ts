ae4f45994eb6b2a3a482572db69723fb9c686767
kmnakane <18519798091@163.com>
2026-01-05 22:42:55 +0800

    删除了顶部导航栏的device

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const siteConfig: SiteConfig = {
 		skills: true, // 技能页面开关
 		timeline: true, // 时间线页面开关
 		albums: true, // 相册页面开关
-		devices: true, // 设备页面开关
+		devices: false, // 设备页面开关
 	},
 
 	// 顶栏标题配置
@@ -283,12 +283,7 @@ export const navBarConfig: NavBarConfig = {
 					url: "/albums/",
 					icon: "material-symbols:photo-library",
 				},
-				{
-					name: "Devices",
-					url: "devices/",
-					icon: "material-symbols:devices",
-					external: false,
-				},
+				
 			],
 		},
 		{
